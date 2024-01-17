7378ef9c4d35b4fab10571139f09518e8df94e63
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-01-17 16:49:50 -0500

    --> Remove warning.

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -3,8 +3,9 @@ Given an array of integers nums and an integer target, return indices of the two
 they add up to target. You may assume that each input would have exactly one solution, and you may
 not use the same element twice. You can return the answer in any order.
 */
+//noinspection RsExternalLinter
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut a: i32;
+    let mut a: i32 = 0;
     let b: i32;
 
     for i in 0..(nums.len() - 1) {
