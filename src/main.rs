9ba713faaff309122ff2e6a04f52cfe6afea4d1d
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-01-16 21:17:01 -0500

    --> First commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use crate::two_sum::two_sum;
+
+mod two_sum;
+
+fn main() {
+    let ans: Vec<i32> = vec![1, 2, 3, 9, 9, 0, 4];
+    let vector:Vec<i32> = two_sum(ans, 19);
+
+    println!("{:?}", vector)
+}
