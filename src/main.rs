e792343f261cb6a0339f4c7e1b294018e6183591
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-01-17 16:50:15 -0500

    --> Updating code.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use crate::two_sum::two_sum;
-
+use crate::palindrome_number::is_palindrome;
 mod two_sum;
+mod palindrome_number;
 
 fn main() {
     let ans: Vec<i32> = vec![1, 2, 3, 9, 9, 0, 4];
     let vector:Vec<i32> = two_sum(ans, 19);
-    println!("{:?}", vector)
+    println!("{:?}", vector);
 
-
+    let a = is_palindrome(1410110141);
+    println!("{}", a);
 }
