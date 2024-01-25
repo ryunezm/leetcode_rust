5c0d30ada3e666c0113a2a01e8269d64f79e55ed
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-01-25 13:46:57 -0500

    --> Updating code.

--- src/easy/two_sum.rs
+++ src/easy/two_sum.rs
@@ -21,11 +21,10 @@ Only one valid answer exists.
 */
 //noinspection RsExternalLinter
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut a: i32;
     let b: i32;
 
     for i in 0..(nums.len() - 1) {
-        a = nums[i];
+        let mut a = nums[i];
 
         for j in i + 1..nums.len() {
             if nums[i] + nums[j] == target {
