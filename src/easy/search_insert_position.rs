80cdb470579286ad22f500df08755fcb8f67d46b
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-02-03 10:19:08 -0500

    --> Added new exercise.

---
+++ src/easy/search_insert_position.rs
@@ -0,0 +1,14 @@
+/*
+35. Search Insert Position
+https://leetcode.com/problems/search-insert-position
+#Array #Binary_Search
+
+Given a sorted array of distinct integers and a target value, return the index if the target is
+found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    0
+}
