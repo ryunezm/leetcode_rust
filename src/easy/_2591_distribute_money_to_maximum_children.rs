c4842b00d8e57ebd64f57b95356108e779d13115
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-02-16 15:23:19 -0500

    --> Adding tests to each exercise.

--- src/easy/_2591_distribute_money_to_maximum_children.rs
+++ src/easy/_2591_distribute_money_to_maximum_children.rs
@@ -27,7 +27,9 @@ pub fn dist_money(money: i32, children: i32) -> i32 {
     let mut cont = 0;
 
 
-    if money == 4 && children == 1 || money_to_dist < 0 { return -1; } else if money_to_dist >= 0 && money_to_dist < 7 { return 0; } else {
+    if money == 4 && children == 1 || money_to_dist < 0 { return -1; }
+    else if money_to_dist >= 0 && money_to_dist < 7 { return 0; }
+    else {
         for i in 7..=money_to_dist {
             if i % 7 == 0 && i <= children * 7 { cont += 1; }
             if i > children * 7 { break; }
@@ -37,4 +39,51 @@ pub fn dist_money(money: i32, children: i32) -> i32 {
         if money_to_dist > children * 7 { cont -= 1; }
     }
     cont
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn dist_money_case_1(){
+        let money = 20;
+        let children = 2;
+        assert_eq!(dist_money(money, children), 1)
+    }
+
+    #[test]
+    fn dist_money_case_2(){
+        let money = 16;
+        let children = 2;
+        assert_eq!(dist_money(money, children), 2)
+    }
+
+    #[test]
+    fn dist_money_case_3(){
+        let money = 4;
+        let children = 1;
+        assert_eq!(dist_money(money, children), -1)
+    }
+
+    #[test]
+    fn dist_money_case_4(){
+        let money = 6;
+        let children = 2;
+        assert_eq!(dist_money(money, children), 0)
+    }
+
+    #[test]
+    fn dist_money_case_5(){
+        let money = 7;
+        let children = 2;
+        assert_eq!(dist_money(money, children), 0)
+    }
+
+    #[test]
+    fn dist_money_case_6(){
+        let money = 7;
+        let children = 2;
+        assert_eq!(dist_money(money, children), 0)
+    }
 }
