92606b44e39ecad488326edddf794945379aced0
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-09-01 11:43:41 -0500

    Added new problem.

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,16 +1,17 @@
-pub(crate) mod _1_two_sum;
-pub(crate) mod _9_palindrome_number;
-pub(crate) mod _13_roman_to_integer;
-pub(crate) mod _118_pascal_triangle;
-pub(crate) mod _342_power_of_four;
-pub(crate) mod _2591_distribute_money_to_maximum_children;
-pub(crate) mod _3019_number_of_changing_keys;
-pub(crate) mod _35_search_insert_position;
-pub(crate) mod _3024_type_of_triangle_ii;
-pub(crate) mod _3028_ant_on_the_boundary;
-pub(crate) mod _387_first_unique_character_in_a_string;
+mod _1_two_sum;
+mod _9_palindrome_number;
+mod _13_roman_to_integer;
+mod _118_pascal_triangle;
+mod _342_power_of_four;
+mod _2591_distribute_money_to_maximum_children;
+mod _3019_number_of_changing_keys;
+mod _35_search_insert_position;
+mod _3024_type_of_triangle_ii;
+mod _3028_ant_on_the_boundary;
+mod _387_first_unique_character_in_a_string;
 //mod _3000_maximum_area_of_longest_diagonal_rectangle;
 //mod _3033_modify_the_matrix;
 mod _2108_find_first_palindromic_string_in_the_array;
 mod _231_power_of_two;
+mod _507_perfect_number;
 
