10efa1a12b8cedcc5c580dfe5eb294d51fa4c63c
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-09-04 14:37:32 -0500

    Added new problem.

---
+++ src/easy/_2520_count_the_digits_that_divide_a_number.rs
@@ -0,0 +1,64 @@
+/*
+2520. Count the Digits That Divide a Number
+https://leetcode.com/problems/count-the-digits-that-divide-a-number
+#Math
+
+
+Given an integer num, return the number of digits in num that divide num.
+
+An integer val divides nums if nums % val == 0.
+
+
+Constraints:
+-> 1 <= num <= 10^9
+-> num does not contain 0 as one of its digits.
+
+*/
+
+
+pub fn count_digits(num: i32) -> i32 {
+    let mut cont = 0;
+    let digits = to_digits(num);
+
+
+    for i in 0.. digits.len() {
+        if (num%digits[i] == 0) {
+            cont = cont + 1;
+        }
+    }
+
+    return cont;
+
+    fn to_digits(num: i32) -> Vec<i32> {
+        num
+            .to_string()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as i32)
+            .collect()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn cn_case_1() {
+        let num = 111;
+        assert_eq!(count_digits(num), 3);
+    }
+
+    #[test]
+    fn cn_case_2() {
+        let num = 121;
+        assert_eq!(count_digits(num), 2);
+    }
+
+    #[test]
+    fn cn_case_3() {
+        let num = 1248;
+        assert_eq!(count_digits(num), 4);
+    }
+}
--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -9,13 +9,8 @@ mod _387_first_unique_character_in_a_string;
 mod _507_perfect_number;
 mod _728_self_dividing_numbers;
 mod _2108_find_first_palindromic_string_in_the_array;
+mod _2520_count_the_digits_that_divide_a_number;
 mod _2591_distribute_money_to_maximum_children;
 mod _3019_number_of_changing_keys;
 mod _3024_type_of_triangle_ii;
 mod _3028_ant_on_the_boundary;
-
-
-
-
-
-
