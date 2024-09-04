d7c8542b6154b21a1c9274f9284e77b1d83df6b5
ryunezm <61928963+ryunezm@users.noreply.github.com>
2024-09-04 11:43:52 -0500

    Added new problem.

---
+++ src/easy/_728_self_dividing_numbers.rs
@@ -0,0 +1,84 @@
+/*
+728. Self Dividing Numbers
+https://leetcode.com/problems/self-dividing-numbers
+
+
+A self-dividing number is a number that is divisible by every digit it contains.
+
+For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+A self-dividing number is not allowed to contain the digit zero.
+
+Given two integers left and right, return a list of all the self-dividing numbers in the range [left, right] (both inclusive).
+
+
+Constraints:
+1 <= left <= right <= 10^4
+*/
+
+pub fn self_dividing_numbers(left: i32, right: i32) -> Vec<i32> {
+    let mut ans = vec![];
+
+    for i in left..right+1 {
+        if has_zero(i) { continue; }
+
+        let mut sum = 0;
+        for x in to_digits(i) {
+            sum = sum + i % x;
+        }
+
+        if sum == 0 {
+            ans.push(i);
+        }
+    }
+
+    return ans;
+
+    fn has_zero(num: i32) -> bool {
+        num
+            .to_string()
+            .contains('0')
+    }
+
+    fn to_digits(num: i32) -> Vec<i32> {
+        num
+            .to_string()
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as i32)
+            .collect()
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sdn_case_1_basic() {
+        let left = 10;
+        let right = 20;
+        assert_eq!(self_dividing_numbers(left, right), [11, 12, 15]);
+    }
+
+    #[test]
+    fn sdn_case_2_digits() {
+        let left= 1;
+        let right= 9;
+        assert_eq!(self_dividing_numbers(left, right), [1,2,3,4,5,6,7,8,9])
+    }
+
+    #[test]
+    fn sdn_case_3_one() {
+        let left = 11;
+        let right = 11;
+        assert_eq!(self_dividing_numbers(left, right), [11]);
+    }
+
+    #[test]
+    fn sdn_case_4_zero(){
+        let left = 49;
+        let right = 50;
+        assert_eq!(self_dividing_numbers(left, right), []);
+    }
+}
--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,17 +1,21 @@
 mod _1_two_sum;
 mod _9_palindrome_number;
 mod _13_roman_to_integer;
+mod _35_search_insert_position;
 mod _118_pascal_triangle;
+mod _231_power_of_two;
 mod _342_power_of_four;
+mod _387_first_unique_character_in_a_string;
+mod _507_perfect_number;
+mod _728_self_dividing_numbers;
+mod _2108_find_first_palindromic_string_in_the_array;
 mod _2591_distribute_money_to_maximum_children;
 mod _3019_number_of_changing_keys;
-mod _35_search_insert_position;
 mod _3024_type_of_triangle_ii;
 mod _3028_ant_on_the_boundary;
-mod _387_first_unique_character_in_a_string;
-//mod _3000_maximum_area_of_longest_diagonal_rectangle;
-//mod _3033_modify_the_matrix;
-mod _2108_find_first_palindromic_string_in_the_array;
-mod _231_power_of_two;
-mod _507_perfect_number;
+
+
+
+
+
 
